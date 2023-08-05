70d39991d90bfd75a04c66b8054a64559ad2db6b
RolandoU548 <rolandou548@gmail.com>
2023-08-04 21:53:06 -0400

    app.js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,29 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#dominio").innerHTML = generarDominio();
+  });
+};
+
+let listaPronombres = ["the", "our", "a", "my"];
+let listaAdj = ["great", "big", "huge", "awesome"];
+let listaSujetos = ["jogger", "racoon", "detective", "squirrel"];
+let listaExtensiones = [".com", ".net", ".us", ".io"];
+let listaDominios = [];
+for (let pronombre of listaPronombres) {
+  for (let extension of listaExtensiones) {
+    for (let sujeto of listaSujetos) {
+      for (let adjetivo of listaAdj) {
+        let nombreDominio = `${pronombre}${adjetivo}${sujeto}${extension}`;
+        listaDominios.push(nombreDominio);
+        console.log(nombreDominio);
+      }
+    }
+  }
+}
+
+let generarDominio = () => {
+  return listaDominios[Math.floor(Math.random() * 256)];
 };
